e1e8c18261078ba775efc23bb6cdc296287b6881
sergeystets <sergeystets@gmail.com>
2021-01-14 01:49:32 +0200

    roll backed avatar attribute

--- src/components/Chat/parts/Chats.vue
+++ src/components/Chat/parts/Chats.vue
@@ -1,7 +1,7 @@
 <template>
   <v-list subheader>
     <v-subheader>Your Chats</v-subheader>
-    <v-list-item v-for="(chat, index) in chats" v-bind:key="chat.name" :to="/chat/ + index">
+    <v-list-item avatar v-for="(chat, index) in chats" v-bind:key="chat.name" :to="/chat/ + index">
       <v-list-item-content>
         <v-list-item-title v-html="chat.name"></v-list-item-title>
       </v-list-item-content>
